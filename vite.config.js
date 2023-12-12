fdcf85abe7ed4c585c229fe2c425a947b41073d3
skj <a916024530@qq.com>
2023-12-12 17:34:33 +0800

    第一次提交

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  // publicPath:'./',
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    // 是否开启 https
+    https: false,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
